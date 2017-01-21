8134aa33bc3479a6c324e12ecee301a9fb3fe1be
werayut.ku@gmail.com <werayut.ku@gmail.com>
2017-01-21 23:13:05 +0700

    header-styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,14 @@
+//Node module import
 import React , {Component} from 'react';
 
+// Component import
+import Header from './Header';
+//Layout component
 class App extends Component {
     render() {
         return(
             <div className="app">
-                HEADER
+                <Header />
                 <div className="container">
                     <h3>{this.props.children}</h3>
                 </div>
---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+// Node module import
+import React, { Component } from 'react';
+
+//Show header
+export default class Header extends Component {
+  render() {
+    return(
+      <div className="header">
+        <div className="container">
+          <div className="brand">
+            Quiz
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
