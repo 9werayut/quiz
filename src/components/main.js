d9a8a63c7a9206678c3a864366b3651978527d47
werayut.ku@gmail.com <werayut.ku@gmail.com>
2017-01-22 23:06:34 +0700

    mapping-state-to-properties

--- src/components/main.js
+++ src/components/main.js
@@ -12,14 +12,38 @@ class Main extends Component {
     componentWillMount(){
         this.props.fetchQuestion();
     }
+    showsQuestion(question){
+        return (
+            <h3>{question === null ? 'data processing' : question.answer}</h3>
+        )
+        if(question === null){
+            return <h3>Data processing...</h3>
+        } else {
+            return (
+                <ul>
+                    <li>answer: {question.answer}</li>
+                    <li>id: {question.id}</li>
+                    <li>description: {question.description}</li>
+                </ul>
+            );
+        }
+    }
     render() {
+        console.log(this.props.question);
         return(
             <div className="main-page">
                 <ActivityLog />
                 <QuestionInfo/>
+                {this.showsQuestion(this.props.question)}
             </div>
         )
     }
 }
 
-export default connect(null, { fetchQuestion })(Main);
+function mapStateToProps(state) {
+    return {
+        question: state.questions.question
+    }
+}
+
+export default connect(mapStateToProps, { fetchQuestion })(Main);
